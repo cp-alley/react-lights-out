3d0d5e53170de24cb05221c5eea836d11fdcc6db
Steven Dowd <swdowd@gmail.com>
2023-09-27 20:11:34 -0400

    Worked on board logic, began work on styling

--- src/Board.js
+++ src/Board.js
@@ -69,21 +69,41 @@ function Board({ nrows=5, ncols=5, chanceLightStartsOn=0.2 }) {
 
       // TODO: Make a (deep) copy of the oldBoard
 
-      for (const row of oldBoard) {
-        [...row]
-      }
+      const newBoard = oldBoard.map(row => [...row]);
 
       // TODO: in the copy, flip this cell and the cells around it
 
+      flipCell(y, x, newBoard);
+      flipCell(y-1, x, newBoard);
+      flipCell(y+1, x, newBoard);
+      flipCell(y, x-1, newBoard);
+      flipCell(y, x-1, newBoard);
+
       // TODO: return the copy
+
+      return newBoard;
     });
   }
 
   // if the game is won, just show a winning msg & render nothing else
+    if (hasWon()) return <p>Congrats! You Won!</p>
 
   // TODO
 
-  // make table board
+  return (
+    <div className="Board" style={{width:`${ncols*100}px`}}>
+      {board.map((row, y) =>
+        row.map((cell, x) =>
+        <Cell key={`${y}-${x}`}
+              isLit={board[y][x]}
+              flipCellsAroundMe={flipCellsAround}/> ))}
+    {/* {for (let y = 0; y < nrows; y++) {
+      for (let x = 0; x < ncols; x++ ){
+        <Cell key={`${y}-${x}`} isLit={board[y][x]} flipCellsAroundMe={flipCellsAround}/>
+      }
+    }} */}
+    </div>
+  )
 
   // TODO
 }
